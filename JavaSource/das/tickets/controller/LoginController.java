2c10607d19804927c2b832feb841fd7d43a3cf0d
Thomas Schlender <tschlender@datameer.com>
2011-05-12 16:39:52 +0200

    DAS-0002: Create a login panel (modal)

--- JavaSource/das/tickets/controller/LoginController.java
+++ JavaSource/das/tickets/controller/LoginController.java
@@ -7,12 +7,33 @@ import javax.faces.context.FacesContext;
 @ManagedBean(name = "loginController")
 public class LoginController {
 
+	private String userName;
+
+	private String password;
+
 	public void display() {
 		System.out.println("!!!!ONLY FOR TESTING!!!!!!!!!!");
 		FacesContext.getCurrentInstance().addMessage("123",
 				new FacesMessage(FacesMessage.SEVERITY_ERROR, "234", "567"));
 		System.out.println(FacesContext.getCurrentInstance().getMessageList()
 				.get(0).getDetail());
+	}
+
+	// getter & setter
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
