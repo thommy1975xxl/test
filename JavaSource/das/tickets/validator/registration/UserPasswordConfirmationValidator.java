4649d7128cc674cc1bc02dc9237d6a090b613c3d
Thomas Schlender <tschlender@datameer.com>
2011-05-24 21:17:29 +0200

    DAS-0025: Refactor registration password confirmation validation

--- JavaSource/das/tickets/validator/registration/UserPasswordConfirmationValidator.java
+++ JavaSource/das/tickets/validator/registration/UserPasswordConfirmationValidator.java
@@ -1,10 +1,13 @@
 package das.tickets.validator.registration;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
+
+import das.tickets.config.RegistrationValidationDefinition;
 
 @FacesValidator(value = "das.tickets.validator.registration.UserPasswordConfirmationValidator")
 public class UserPasswordConfirmationValidator implements Validator {
@@ -14,18 +17,17 @@ public class UserPasswordConfirmationValidator implements Validator {
 			Object value) throws ValidatorException {
 
 		if (value instanceof String) {
-			// UIComponent passwordComponent = facesContext.getViewRoot()
-			// .findComponent("j_idt17:passwordInput");
-			// String passwordValue = (String)
-			// ((org.primefaces.component.password.Password) passwordComponent)
-			// .getValue();
-			// if (!value.equals(passwordValue)) {
-			// FacesMessage facesMessage = new FacesMessage(
-			// FacesMessage.SEVERITY_ERROR,
-			// RegistrationValidationDefinition.MESSAGE_PASSWORD_CONFIRMATION,
-			// null);
-			// throw new ValidatorException(facesMessage);
-			// }
+			UIComponent passwordComponent = facesContext.getViewRoot()
+					.findComponent(":registrationForm:passwordInput");
+			String passwordValue = (String) ((org.primefaces.component.password.Password) passwordComponent)
+					.getValue();
+			if (!value.equals(passwordValue)) {
+				FacesMessage facesMessage = new FacesMessage(
+						FacesMessage.SEVERITY_ERROR,
+						RegistrationValidationDefinition.MESSAGE_PASSWORD_CONFIRMATION,
+						null);
+				throw new ValidatorException(facesMessage);
+			}
 
 		}
 
